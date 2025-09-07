45d54481b8da960c0e8129938a6c387a316b8fab
vaibhav164 <vai1641996@gmail.com>
2025-09-08 03:19:41 +0530

    Rotate Animation

--- src/Screens/Animations.tsx
+++ src/Screens/Animations.tsx
@@ -1,18 +1,27 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Animated, StyleSheet, Text } from 'react-native'
 
 function Animations() {
     const fadeAnimationRef = useRef((new Animated.Value(0))).current;
-
     useEffect(() => {
         Animated.timing(fadeAnimationRef, {
-            toValue: 1,
-            duration: 1000,
-            useNativeDriver: true
+            toValue: 10000000,
+            useNativeDriver: true,
+            duration: 30000
         }).start();
     }, [fadeAnimationRef])
+
+    // const translateX = fadeAnimationRef.interpolate({
+    //     inputRange: [0, 100],
+    //     outputRange: [0, 400],
+    // });
     return (
-        <Animated.View style={{ ...styles.container, opacity: fadeAnimationRef }}>
+        <Animated.View style={{
+            ...styles.container, transform: [
+                // { translateX },
+                { rotate: fadeAnimationRef.interpolate({ inputRange: [0, 1], outputRange: ['0deg', '360deg'] }) }
+            ]
+        }}>
             <Text>{"My First Animation Component"}</Text>
         </Animated.View>
     )
