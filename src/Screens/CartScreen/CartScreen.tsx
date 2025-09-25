647099b401a1bb769464fde01c4cbe5e2f3ad7c5
vaibhav164 <vai1641996@gmail.com>
2025-09-25 17:21:45 +0530

    Implemented Blank Cart Screen

---
+++ src/Screens/CartScreen/CartScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Header from '../../Components/Header'
+
+export default function CartScreen() {
+  return (
+    <View>
+        <Header title='CartScreen'/>
+    </View>
+  )
+}
